76f566813ea1a7533e5aacee41d7cfb236fb64ae
venom <venom@DESKTOP-VI6EVSE>
2020-07-17 15:11:02 -0500

    Program N°2.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace POOBasicoPractica2
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            //Instanciacion
+            Vehiculo vehiculo1 = new Vehiculo();
+
+            vehiculo1.Motor = "Apagado";
+            vehiculo1.TanqueGasolina = 5;
+            vehiculo1.Velocidad = 0;
+            
+
+            Console.WriteLine(vehiculo1.Motor);
+            vehiculo1.Disminuir();
+
+            Console.WriteLine(vehiculo1.Velocidad);
+            vehiculo1.Frenar();
+
+            Console.WriteLine(vehiculo1.Velocidad);
+            vehiculo1.Frenar();
+
+            Console.WriteLine(vehiculo1.TanqueGasolina);
+        }
+    }
+}
---
+++ Vehiculo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace POOBasicoPractica2
+{
+    class Vehiculo
+    {
+        //Propiedades
+        public int Velocidad { get; set; }
+        public string Motor { get; set; }
+        public int TanqueGasolina { get; set; }
+
+        //Métodos
+        public int Aumentar()
+        {
+            if (Velocidad >= 0 && Velocidad <= 200) 
+            {
+                Velocidad += 10;
+                return Velocidad;
+            }else{
+                return 200;
+            }
+        }
+
+        public int Disminuir ()
+        {
+            if(Velocidad>=0)
+            {
+                Velocidad -= 5;
+            }
+            return Velocidad;
+        }
+        public int Frenar()
+        {
+            if(Velocidad>=0)
+            {
+                Velocidad = 0;
+            }
+            return Velocidad;
+        }
+      
+    }
+}
